809fd1e9d8fdbef2708d2a21decf09b1a0e250b8
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2016-05-01 18:43:25 +0200

    Add Edit Event button.

--- src/pluton/event/controllers.py
+++ src/pluton/event/controllers.py
@@ -6,5 +6,9 @@ class HideEvent(JsonController):
     def make(self):
         event = self.events.get_by_id(id=self.matchdict['event_id'])
         event.is_hidden = True
+        self.utils.redirect(
+            'endpoints:show',
+            endpoint_id=event.endpoint_id,
+        )
+
         self.database().commit()
-        self.utils.redirect('dashboard')
